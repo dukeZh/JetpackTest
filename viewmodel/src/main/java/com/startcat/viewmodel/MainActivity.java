056912af117b6dc18a7a0e68f82e4ea28500ac47
zhaihai <duke_zh@yeah.net>
2021-11-27 10:52:06 +0800

    init viewModel

--- viewmodel/src/main/java/com/startcat/viewmodel/MainActivity.java
+++ viewmodel/src/main/java/com/startcat/viewmodel/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * @author duke_zh
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView textview;
--- viewmodel/src/main/java/com/startcat/viewmodel/MyViewModel.java
+++ viewmodel/src/main/java/com/startcat/viewmodel/MyViewModel.java
@@ -1,7 +1,17 @@
 package com.startcat.viewmodel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
-public class MyViewModel extends ViewModel {
+
+//如果使用context 使用 AndroidViewModel，否则使用ViewModel
+public class MyViewModel extends AndroidViewModel{
     public int number;
+
+    public MyViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
